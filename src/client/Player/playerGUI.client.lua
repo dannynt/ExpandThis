ab989d202b96536213aa8bb166d84ac6a4aaced6
Daniel Nael <daniel@joyixir.com>
2023-03-20 19:20:25 +0200

    Add player notification

---
+++ src/client/Player/playerGUI.client.lua
@@ -0,0 +1,7 @@
+local StarterGui = game:GetService("StarterGui")
+
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Danger Zones expanding!";
+	Text = "Use white spheres to decrease their scale. There is no winning in this game - stay alive as long as possible.";
+	Icon = ""})
+Duration = 60;
