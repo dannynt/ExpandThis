5785b8313200627c9b7f6f96c2fff99ae7dd43fb
Daniel Nael <daniel@joyixir.com>
2023-03-20 10:39:00 +0200

    Add make expander kill player and reset game

--- src/server/Expander/expander.server.lua
+++ src/server/Expander/expander.server.lua
@@ -12,13 +12,36 @@ local growTime = 5
 local growWaitTimeFactor = 1.5
 local size = 0
 local expanding = true
+local tweenInfo
+local sizeTween
 
 local function expand()
     size += growthRate;
-    local tweenInfo = TweenInfo.new(growTime, Enum.EasingStyle.Quad)
-    local sizeTween = TweenService:Create(expander, tweenInfo, {Size = Vector3.new(size, size, size)})
+    tweenInfo = TweenInfo.new(growTime, Enum.EasingStyle.Quad)
+    sizeTween = TweenService:Create(expander, tweenInfo, {Size = Vector3.new(size, size, size)})
     sizeTween:Play()
 end
+
+local function reset()
+    size = 0
+    expander.Size = Vector3.zero
+    sizeTween:Cancel()
+end
+
+local function killPlayer(player)
+    local humanoid = player.Parent:FindFirstChild("Humanoid")
+    humanoid.Health = 0
+    print(humanoid.Name .. " killed")
+end
+
+expander.Touched:Connect(function(other)
+    if other.Parent:FindFirstChild("Humanoid") then
+        killPlayer(other.Parent:FindFirstChild("Humanoid"))
+        reset()
+    end
+end)
+
+print("Aaa")
 
 while expanding do
     expand()
