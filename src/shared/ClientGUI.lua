e2faa402b05149f7e94d162cd213268ea256aed8
Daniel Nael <daniel@joyixir.com>
2023-03-28 20:54:38 +0300

    Fix ClientGUI bug

--- src/shared/ClientGUI.lua
+++ src/shared/ClientGUI.lua
@@ -1,18 +1,17 @@
 local ClientGUI = {}
-
-local TextLabel = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("ScreenGui").StatusText
 local _targetRotation = -2
 
 game.ReplicatedStorage.NotificationHandler.OnClientEvent:Connect((function(text, text2)
-    --local Clone = TextLabel:Clone()
+    local _textLabel = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("ScreenGui").StatusText
+    --local Clone = _textLabel:Clone()
     --Clone.Text = text
-    --Clone.Parent = TextLabel.Parent
+    --Clone.Parent = _textLabel.Parent
     --wait(5)
     --Clone:Destroy()
-    TextLabel.Text = text;
+    _textLabel.Text = text;
 
     local tweenInfo = TweenInfo.new(2)
-    local tween = game:GetService("TweenService"):Create(TextLabel, tweenInfo, {Rotation = _targetRotation})
+    local tween = game:GetService("TweenService"):Create(_textLabel, tweenInfo, {Rotation = _targetRotation})
     _targetRotation = _targetRotation * -1
 
     tween:Play()
