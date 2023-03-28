b15dd8889d254869c6361204901c946f71508e68
Daniel Nael <daniel@joyixir.com>
2023-03-28 09:38:29 +0300

    Add Spawner to module scripts

---
+++ src/shared/Spawner.lua
@@ -0,0 +1,32 @@
+local Spawner = {}
+
+function Spawner.new(coolDownTime)
+    local self = setmetatable({}, {__index = Spawner})
+    self._coolDownTime = coolDownTime
+    self._active = false
+    self._spawnPoints = game.Workspace.SpawnPoints:GetChildren()
+    return self
+end
+
+function Spawner:Clone()
+    local randomGroundIdx = math.random(1, #self._spawnPoints)
+    print(randomGroundIdx)
+    local bullet = game.ReplicatedStorage.Assets.Bullet
+    local clone = bullet:Clone()
+    clone.Parent = game.Workspace
+    clone.Position = self._spawnPoints[randomGroundIdx].Position
+end
+
+function Spawner:Start()
+    self._active = true
+    while self._active do
+        self:Clone()
+        wait(self._coolDownTime)
+    end
+end
+
+function Spawner:Stop()
+    self._active = false
+end
+
+return Spawner
