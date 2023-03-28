36f21f4674bc2e378170331ce28d5fb6184f1462
Daniel Nael <daniel@joyixir.com>
2023-03-28 10:31:59 +0300

    Add match script

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,15 +1,5 @@
 print("Starting server")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Expander = require(ReplicatedStorage:WaitForChild("Expander")).new(15, 15, 5, 1.5)
-local Spawner = require(ReplicatedStorage:WaitForChild("Spawner")).new(2)
+local Match = require(ReplicatedStorage:WaitForChild("Match")).new()
 
-local spawnerCoroutine = coroutine.create(function()
-    Spawner:Start()
-end)
-
-local expanderCoroutine = coroutine.create(function()
-    Expander:StartExpandLoop()
-end)
-
-coroutine.resume(spawnerCoroutine)
-coroutine.resume(expanderCoroutine)
+Match:Start()
---
+++ src/shared/Match.lua
@@ -0,0 +1,34 @@
+local Match = {}
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+function Match.new()
+    local self = setmetatable({}, {__index = Match})
+    self._active = false
+    self._expander = require(ReplicatedStorage:WaitForChild("Expander")).new(15, 15, 5, 1.5)
+    self._spawner = require(ReplicatedStorage:WaitForChild("Spawner")).new(2)
+    self._spawnerCoroutine = coroutine.create(function()
+        self._spawner:Start()
+    end)
+    self._expanderCoroutine = coroutine.create(function()
+        self._expander:StartExpandLoop()
+    end)
+
+    return self
+end
+
+function Match:Start()
+    self._active = true
+    coroutine.resume(self._spawnerCoroutine)
+    coroutine.resume(self._expanderCoroutine)
+end
+
+function Match:End()
+    self._active = false
+    coroutine.yield(self._spawnerCoroutine)
+    coroutine.yield(self._expanderCoroutine)
+    self._expander:Reset()
+    self._spawner:Stop()
+end
+
+return Match
