492ef85ede6d013e75b76613cb339517b66d06c4
Daniel Nael <daniel@joyixir.com>
2023-03-28 09:38:59 +0300

    Create initial coroutines for Expander and Spawner for server

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,6 +1,15 @@
--- 15 15 5 1.5
 print("Starting server")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Expander = require(ReplicatedStorage:WaitForChild("Expander")).new()
-print(Expander)
-Expander:StartExpandLoop()
+local Expander = require(ReplicatedStorage:WaitForChild("Expander")).new(15, 15, 5, 1.5)
+local Spawner = require(ReplicatedStorage:WaitForChild("Spawner")).new(2)
+
+local spawnerCoroutine = coroutine.create(function()
+    Spawner:Start()
+end)
+
+local expanderCoroutine = coroutine.create(function()
+    Expander:StartExpandLoop()
+end)
+
+coroutine.resume(spawnerCoroutine)
+coroutine.resume(expanderCoroutine)
